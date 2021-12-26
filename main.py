03897cf170da88b098e1594fbd6c04ae0436caae
carnoldcoding <carnold1@ithaca.edu>
2021-12-26 15:59:48 -0500

    Added functionality with sending messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scraper import *
 from fileIO import *
 from messenger import *
+import os
 
 url = "https://www.epicnpc.com/forums/last-cloudia-accounts.1797/"
 file_url = "./output.csv"
@@ -9,10 +10,12 @@ file_url = "./output.csv"
 def main():
     document = collect(url)
     listings = get_listings(document)
-    new_urls = compare(read_urls(file_url), only_urls(listings))  # Compare what's in the file to what was pulled from the web
+    new_urls = compare(read_urls(file_url),
+                       only_urls(listings))  # Compare what's in the file to what was pulled from the web
 
-    if len(new_urls) > 0:
-        send_message(new_urls, listings)
+    send_message("hello")
+
     # write_urls(listings)
+
 
 main()
--- messenger.py
+++ messenger.py
@@ -1,3 +1,51 @@
+import email, smtplib, ssl
+from providers import PROVIDERS
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-def send_message(new_urls, listings):
-    print("fuck")
+import os
+from os.path import basename
+
+
+def message_config(number: str, message: str, file_path: str, mime_maintype: str, mime_subtype: str, provider: str, sender_credentials: tuple, subject: str = "sent using py",
+                 smtp_server="smtp.gmail.com", smtp_port: int = 465):
+    sender_email, email_password = sender_credentials
+    receiver_email = f"{number}@{PROVIDERS.get(provider).get('mms')}"
+
+
+    email_message = MIMEMultipart()
+    email_message["Subject"] = subject
+    email_message["To"] = receiver_email
+    email_message["From"] = sender_email
+
+    email_message.attach(MIMEText(message, "plain"))
+
+    with open(file_path, "rb") as attachment:
+        part = MIMEBase(mime_maintype, mime_subtype)
+        part.set_payload(attachment.read())
+
+        encoders.encode_base64(part)
+        part.add_header(
+            "Content-Disposition", f"attachment; filename={basename(file_path)}"
+        )
+
+        email_message.attach(part)
+
+        text = email_message.as_string()
+
+    with smtplib.SMTP_SSL(smtp_server, smtp_port, context=ssl.create_default_context()) as email:
+        email.login(sender_email, email_password)
+        email.sendmail(sender_email, receiver_email, text)
+
+
+def send_message(message):
+    number = os.environ["CELL_NUMBER"]
+    message = message
+    provider = "Verizon"
+    file_path = "output.csv"
+    mime_maintype = "text"
+    mime_subtype = "csv"
+    sender_credentials = (os.environ["BOT_EMAIL"], os.environ["BOT_EMAIL_CODE"])
+    message_config(number, message, file_path, mime_maintype, mime_subtype, provider, sender_credentials)
